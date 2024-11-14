9eca3e17f3c938fcc155803692377c739ed4a900
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-11-14 10:43:59 +0100

    DONE: exercice 9

---
+++ Exercice-9/script.js
@@ -0,0 +1,13 @@
+// const word = "bonjour";
+// let reversedWord = "";
+
+// for (let i = word.length - 1; i >= 0; i -= 1) {
+//     reversedWord += word[i];
+// }
+
+// console.log(reversedWord);
+
+const word = "bonnjour";
+let reversedWord = word.split("").reverse().join("");
+
+console.log(reversedWord);
